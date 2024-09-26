222b21700d2d0b0691b0a25e6b8bab99f79b915e
ezhicham <zghariservices@gmail.com>
2024-09-27 00:46:23 +0100

    cpa link test

--- src/app/page.js
+++ src/app/page.js
@@ -19,6 +19,7 @@ export default function Home() {
           <a target="_blank" href="https://smrturl.co/o/578434/53489682?s1=">offer shien</a>
           <a  target="_blank"  href="https://smrturl.co/o/578434/53431476?s1=">offer ukraine</a>
           <a href="https://smrturl.co/o/578434/53449849?s1="> canada kfc</a>
+        <a href="https://smrturl.co/o/578434/53491437?s1=">usa offer :53491437</a>
           <p>🚀 Crypto World: Learn, Earn, Invest! | 💡 Master the crypto market with us and unlock endless opportunities! 💰 Start earning, grow your wealth, 
             and invest smartly with the best platform. 🏆 Join the revolution today! 🌟
           .</p>
